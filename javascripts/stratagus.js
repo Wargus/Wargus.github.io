4e2d0dd03fa526566ec17920b01c8c1de0ebcf5d
mimooh <mimoohowy@gmail.com>
2016-03-06 16:29:52 +0100

    common menu on top via js, some css, info that stratagus is autoinstalled

---
+++ javascripts/stratagus.js
@@ -0,0 +1,8 @@
+$(function()  { 
+	$("#menu").html("\
+		<a href=index.html>Stratagus</a> \
+		<a href=wargus.html>Wargus</a> \
+		<a href=war1gus.html>War1gus</a> \
+		<a href=aleona.html>Aleonas Tales</a>\
+	");
+});
