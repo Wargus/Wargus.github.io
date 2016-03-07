802744410c596bc3dc486c1ea8dc2bf331ad539e
mimooh <mimoohowy@gmail.com>
2016-03-07 21:44:20 +0100

    vintage separator, css

--- javascripts/stratagus.js
+++ javascripts/stratagus.js
@@ -4,6 +4,7 @@ $(function()  {
 		<a href=war1gus.html>War1gus</a> \
 		<a href=aleona.html>Aleona's Tales</a> \
 		<a href=stratagus.html>Stratagus</a> \
+		<br><img src=img/vintage_hr.svg> \
 	");
 
 	$("#dlLinux").mouseenter(function() { $("#dlLinuxInfo").slideDown("slow"); });
