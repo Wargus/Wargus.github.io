9510eee1ff2b4df087cc7633d4a10416348af270
Tim Felgentreff <timfelgentreff@gmail.com>
2017-06-08 13:30:06 +0200

    list public games

--- javascripts/stratagus.js
+++ javascripts/stratagus.js
@@ -1,33 +1,62 @@
-$(function()  { 
-	$("#menu").html("\
-		<a href=index.html>Wargus</a> \
-		<a href=war1gus.html>War1gus</a> \
-		<a href=aleona.html>Aleona's Tales</a> \
-		<a href=wyrmsun.html>Wyrmsun</a> \
-		<a href=stratagus.html>Stratagus</a> \
-		<a href=faq.html>FAQ</a> \
-	");
+$(function()  {
+    $("#menu").html("\
+        <a href=index.html>Wargus</a> \
+        <a href=war1gus.html>War1gus</a> \
+        <a href=aleona.html>Aleona's Tales</a> \
+        <a href=wyrmsun.html>Wyrmsun</a> \
+        <a href=stratagus.html>Stratagus</a> \
+        <a href=faq.html>FAQ</a> \
+    ");
 
-	$("#dlLinuxStable").mouseenter(function() { 
-		$("#dlLinuxUnstableInfo").slideUp("slow"); 
-		$("#dlLinuxStableInfo").slideDown("slow"); 
-	});
-	$("#dlLinuxUnstable").mouseenter(function() { 
-		$("#dlLinuxStableInfo").slideUp("slow"); 
-		$("#dlLinuxUnstableInfo").slideDown("slow"); 
-	});
+    $("#dlLinuxStable").mouseenter(function() {
+        $("#dlLinuxUnstableInfo").slideUp("slow");
+        $("#dlLinuxStableInfo").slideDown("slow");
+    });
+    $("#dlLinuxUnstable").mouseenter(function() {
+        $("#dlLinuxStableInfo").slideUp("slow");
+        $("#dlLinuxUnstableInfo").slideDown("slow");
+    });
 
-	// $(".dlNotLinux").mouseenter(function() { 
-	// 	$("#dlLinuxStableInfo").slideUp("slow"); 
-	// 	$("#dlLinuxUnstableInfo").slideUp("slow"); 
-	// });
+    // $(".dlNotLinux").mouseenter(function() {
+    //  $("#dlLinuxStableInfo").slideUp("slow");
+    //  $("#dlLinuxUnstableInfo").slideUp("slow");
+    // });
 
-	$("body").on("click", ".closeImg",function() {
-		$("#dlLinuxStableInfo").slideUp("fast"); 
-		$("#dlLinuxUnstableInfo").slideUp("fast"); 
-	});
+    $("body").on("click", ".closeImg",function() {
+        $("#dlLinuxStableInfo").slideUp("fast");
+        $("#dlLinuxUnstableInfo").slideUp("fast");
+    });
 
+    function printJSON(data) {
+        var jsonDiv = document.createElement("div");
+        for (var key in data) {
+            var value = data[key];
+            var keyDiv = document.createElement("div");
+            var nameDiv = document.createElement("div");
+            nameDiv.innerText = key;
+            nameDiv.classList.add("key");
+            keyDiv.appendChild(nameDiv);
+            if (typeof(value) == "object") {
+                keyDiv.appendChild(printJSON(value));
+            } else {
+                var valueSpan = document.createElement("span");
+                valueSpan.innerText = value;
+                keyDiv.appendChild(valueSpan);
+            }
+            keyDiv.classList.add(key);
+            jsonDiv.appendChild(keyDiv);
+        }
+        return jsonDiv;
+    }
 
-
-
+    function getMetaServers() {
+        $.getJSON("https://metaservernet.herokuapp.com/", function (data) {
+            var metaserverDivs = printJSON(data);
+            var parentDiv = $("#metaservers");
+            parentDiv.empty();
+            parentDiv.append(metaserverDivs);
+        });
+        setTimeout(getMetaServers, 5000);
+    }
+    setTimeout(getMetaServers, 1000);
 });
